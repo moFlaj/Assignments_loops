553974cc3be75e210586c6f0852f0f0c52c96613
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-04-20 14:50:29 +0100

    Create Patterns3.java

---
+++ Patterns3.java
@@ -0,0 +1,45 @@
+public class Patterns3{
+
+		public static void main(String[] args){
+		int number = 1;
+		int number2 = 1;
+
+		for(int counter = 1; counter <= 6; counter++ ){
+
+			if(counter==6){
+			System.out.print("");}
+			else if(counter==5){
+			System.out.print("	");}
+			else if(counter==4){
+			System.out.print("		");}
+			else if(counter==3){
+			System.out.print("			");}
+			else if(counter==2){
+			System.out.print("				");}
+			else if(counter==1){
+			System.out.print("					");}
+
+			System.out.printf("%d	", number);
+
+			if(counter >= 2){
+			
+			for(int counter1 = 1; counter1 < counter ; counter1++){
+
+			System.out.printf("%d	", ++number2);
+
+			
+			}
+			
+			}
+			
+			
+			
+			number2 = 1;
+			System.out.println();}
+			
+			
+
+
+}
+	
+}
