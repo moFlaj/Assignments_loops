c4ad539a4cbb309df5ab980316e75c996f92a002
moFlaj <141844110+moFlaj@users.noreply.github.com>
2025-04-20 14:10:36 +0100

    Create Patterns1.java
    Pattern two

---
+++ Patterns1.java
@@ -0,0 +1,30 @@
+public class Patterns1{
+
+		public static void main(String[] args){
+		int number = 1;
+		int number2 = 1;
+
+		for(int counter = 6; counter > 0; counter--){
+		System.out.printf("%d	", number);
+
+		
+			if(counter <= 6){
+
+			for(int counter1 = 1; counter1 < counter ; counter1++){
+
+			System.out.printf("%d	", number2++);
+
+			
+			}}
+
+			
+			number2 = 2;
+			System.out.println();}
+			
+			
+
+
+
+}
+	
+}
