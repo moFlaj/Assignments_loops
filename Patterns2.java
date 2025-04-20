ffe2087132ca2217bf10e554181a7743850154bd
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-04-20 14:41:01 +0100

    Create Patterns2.java

---
+++ Patterns2.java
@@ -0,0 +1,54 @@
+public class Patterns2{
+
+		public static void main(String[] args){
+		int number = 1;
+		int number2 = 1;
+
+		for(int counter = 6; counter > 0; counter--){
+		if(counter==5){
+			System.out.print("	");}
+			else if(counter==4){
+			System.out.print("		");}
+			else if(counter==3){
+			System.out.print("			");}
+			else if(counter==2){
+			System.out.print("				");}
+			else if(counter==1){
+			System.out.print("					");
+			}
+
+			System.out.printf("%d	", number);
+
+			if(counter <= 6){
+			
+			for(int counter1 = 1; counter1 < counter ; counter1++){
+
+			System.out.printf("%d	", ++number2);
+
+			
+			}
+			
+			}
+			if(counter==5){
+			System.out.print("	");}
+			else if(counter==4){
+			System.out.print("		");}
+			else if(counter==3){
+			System.out.print("			");}
+			else if(counter==2){
+			System.out.print("				");}
+			else if(counter==1){
+			System.out.print("					");
+			}
+			
+			
+			
+			number2 = 1;
+			System.out.println();}
+			
+			
+
+
+}
+	
+}
