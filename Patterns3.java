5157bc83c4a620c2d26d5d88a6cb474deff1f9e3
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-04-24 13:57:59 +0100

    Update Patterns3.java

--- Patterns3.java
+++ Patterns3.java
@@ -1,14 +1,12 @@
+
 public class Patterns3{
 
 		public static void main(String[] args){
 		int number = 1;
 		int number2 = 1;
 
-		for(int counter = 1; counter <= 6; counter++ ){
-
-			if(counter==6){
-			System.out.print("");}
-			else if(counter==5){
+		for(int counter = 6; counter > 0; counter--){
+			if(counter==5){
 			System.out.print("	");}
 			else if(counter==4){
 			System.out.print("		");}
@@ -17,22 +15,20 @@ public class Patterns3{
 			else if(counter==2){
 			System.out.print("				");}
 			else if(counter==1){
-			System.out.print("					");}
+			System.out.print("					");
+			}
 
 			System.out.printf("%d	", number);
 
-			if(counter >= 2){
+			if(counter <= 6){
 			
 			for(int counter1 = 1; counter1 < counter ; counter1++){
 
 			System.out.printf("%d	", ++number2);
-
 			
 			}
 			
 			}
-			
-			
 			
 			number2 = 1;
 			System.out.println();}
@@ -43,3 +39,4 @@ public class Patterns3{
 }
 	
 }
+
