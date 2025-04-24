e7303dbbd4f141c6765350bc56e06397412938e9
Adebola Folorunsho Ayomide <141844110+moFlaj@users.noreply.github.com>
2025-04-24 13:52:32 +0100

    Update Patterns2.java

--- Patterns2.java
+++ Patterns2.java
@@ -1,50 +1,36 @@
 public class Patterns2{
 
 		public static void main(String[] args){
-		int number = 1;
-		int number2 = 1;
 
-		for(int counter = 6; counter > 0; counter--){
-		if(counter==5){
-			System.out.print("	");}
-			else if(counter==4){
-			System.out.print("		");}
-			else if(counter==3){
-			System.out.print("			");}
-			else if(counter==2){
-			System.out.print("				");}
-			else if(counter==1){
-			System.out.print("					");
-			}
+		int number;
 
-			System.out.printf("%d	", number);
-
-			if(counter <= 6){
+		for(int counter = 1; counter <= 6; counter++){
+		
+			number = counter;
+			if(counter == 1){
+				System.out.print("					");}
+			else if(counter == 2){
+				System.out.print("				");}
+			else if(counter == 3){
+				System.out.print("			");}
+			else if(counter == 4){
+				System.out.print("		");}
+			else if(counter == 5){
+				System.out.print("	");}
+			else if(counter == 6){
+				System.out.print("");}
 			
-			for(int counter1 = 1; counter1 < counter ; counter1++){
-
-			System.out.printf("%d	", ++number2);
-
 			
-			}
+			for(int counter1 = 0; counter1 < counter ; counter1++){
+
+			System.out.printf("%d	", number--);
+
 			
 			}
-			if(counter==5){
-			System.out.print("	");}
-			else if(counter==4){
-			System.out.print("		");}
-			else if(counter==3){
-			System.out.print("			");}
-			else if(counter==2){
-			System.out.print("				");}
-			else if(counter==1){
-			System.out.print("					");
+			System.out.println();
+
 			}
 			
-			
-			
-			number2 = 1;
-			System.out.println();}
 			
 			
 
